7e792a63afc85b3b49d7fe546443ba27d96915dc
Alex <alexandersychev2005@gmail.com>
2025-12-14 19:38:13 +0200

    Added files

---
+++ model.py
@@ -0,0 +1,116 @@
+import math
+
+import torch
+import torch.nn as nn
+
+
+from torch_geometric.nn import GCNConv, global_mean_pool
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int = 5000, dropout: float = 0.1):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+
+        # Create a vector of shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(
+            1
+        )  # (Seq_len, 1)
+        # Compute the positional encodings once in log space.
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )
+        # Apply the sin to even positions
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # Apply the cos to odd positions
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)  # (1, Seq_len, d_model) batch dimension
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        # x: [batch_size, seq_len, d_model]
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
+
+
+
+class LigandGNN(nn.Module):
+    def __init__(self, input_dim, hidden_channels):
+        super().__init__()
+        self.hidden_channels = hidden_channels
+
+        self.conv1 = GCNConv(input_dim, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x, edge_index, batch):
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.dropout(x)
+
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+        x = self.dropout(x)
+
+        # Averaging nodes and got the molecula vector
+        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
+        return x
+
+class ProteinTransformer(nn.Module):
+    def __init__(self, vocab_size, d_model=128, N=2, h=4, output_dim=128):
+        super().__init__()
+        self.d_model = d_model
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.pos_encoder = PositionalEncoding(d_model)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=h, batch_first=True)
+        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=N)
+
+        self.fc = nn.Linear(d_model, output_dim)
+
+    def forward(self, x):
+        # x: [batch_size, seq_len]
+        padding_mask = (x == 0) # mask for PAD tokens
+        x = self.embedding(x) * math.sqrt(self.d_model)
+        x = self.pos_encoder(x)
+        x = self.transformer(x, src_key_padding_mask=padding_mask)
+
+        mask = (~padding_mask).float().unsqueeze(-1)
+        x = x * mask
+
+        sum_x = x.sum(dim=1)  # Global average pooling
+        token_counts = mask.sum(dim=1).clamp(min=1e-9)
+        x = sum_x / token_counts
+        x = self.fc(x)
+        return x
+
+class BindingAffinityModel(nn.Module):
+    def __init__(self, num_node_features, hidden_channels_gnn):
+        super().__init__()
+        # Tower 1 - Ligand GNN
+        self.ligand_gnn = LigandGNN(input_dim=num_node_features, hidden_channels=hidden_channels_gnn)
+        # Tower 2 - Protein Transformer
+        self.protein_transformer = ProteinTransformer(vocab_size=26)
+
+        self.head = nn.Sequential(
+            nn.Linear(128 + 128, 256),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(256, 1),
+        )
+    def forward(self, x, edge_index, batch, protein_seq):
+        ligand_vec = self.ligand_gnn(x, edge_index, batch)
+        batch_size = batch.max().item() + 1
+        protein_seq = protein_seq.view(batch_size, -1)
+
+        protein_vec = self.protein_transformer(protein_seq)
+        combined = torch.cat([ligand_vec, protein_vec], dim=1)
+        return self.head(combined)
+
+
---
+++ train.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import pandas as pd
+from torch.utils.data import random_split
+from torch_geometric.loader import DataLoader
+from dataset import BindingDataset
+from model import BindingAffinityModel
+from tqdm import tqdm
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def train_epoch(model, loader, optimizer, criterion):
+    model.train()
+    total_loss = 0
+    for batch in tqdm(loader, desc="Training"):
+        batch = batch.to(DEVICE)
+        optimizer.zero_grad()
+
+        out = model(batch.x, batch.edge_index, batch.batch, batch.protein_seq)
+        loss = criterion(out.squeeze(), batch.y.squeeze())
+
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(loader)
+
+def evaluate(model, loader, criterion):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for batch in loader:
+            batch = batch.to(DEVICE)
+            out = model(batch.x, batch.edge_index, batch.batch, batch.protein_seq)
+            loss = criterion(out.squeeze(), batch.y.squeeze())
+            total_loss += loss.item()
+    return total_loss / len(loader)
+
+def main():
+    # Load dataset
+    dataframe = pd.read_csv('pdbbind_refined_dataset.csv')
+    dataframe.dropna(inplace=True)
+    print("Dataset loaded with {} samples".format(len(dataframe)))
+    dataset = BindingDataset(dataframe)
+    print("Dataset transformed with {} samples".format(len(dataset)))
+
+
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+    num_features = train_dataset[0].x.shape[1]
+    print("Number of node features:", num_features)
+
+    model = BindingAffinityModel(num_node_features=num_features, hidden_channels_gnn=128).to(DEVICE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0005, weight_decay=1e-4)
+    criterion = nn.MSELoss()
+
+    num_epochs = 20
+    for epoch in range(num_epochs):
+        train_loss = train_epoch(model, train_loader, optimizer, criterion)
+        test_loss = evaluate(model, test_loader, criterion)
+        print(f'Epoch {epoch+1}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}')
+    torch.save(model.state_dict(), './model.pth')
+
+
+if __name__ == "__main__":
+    main()
